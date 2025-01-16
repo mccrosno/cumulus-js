bc73d450b0a8ab4171eef70dfc1674260855c599
LostSpaghetti <mccrosno@gmail.com>
2025-01-16 14:03:02 -0600

    Revert "Updated tailwind config"
    This reverts commit a1d05023aa608ba1720d56be2e1a54823aba662b.

--- cumulus/tailwind.config.ts
+++ cumulus/tailwind.config.ts
@@ -5,7 +5,6 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/styles/**/*.css",
   ],
   theme: {
     extend: {
