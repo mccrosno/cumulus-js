8bd06d7bdd06634b824c0cb16fbd12f3a68d56b1
LostSpaghetti <mccrosno@gmail.com>
2025-01-30 13:34:22 -0600

    Updated glassmorphic.tsx to fix static only issue Added parameters that define whether a certain element should be customized from a default value. If yes, then the parameter will be defined within the className instead of the function.

--- cumulus/app/utils/glassmorphic.tsx
+++ cumulus/app/utils/glassmorphic.tsx
@@ -1,8 +1,25 @@
-const glassmorphic = () => {
+const glassmorphic = ({
+    customBGColor,
+    customBlurAmt,
+    customShadowColor,
+    customShadowSpread,
+    customGradientStartColor,
+    customGradientEndColor,
+    customZIndex,
+}:{
+    customBGColor?: boolean,
+    customBlurAmt?: boolean,
+    customShadowColor?: boolean,
+    customShadowSpread?: boolean,
+    customGradientStartColor?: boolean,
+    customGradientEndColor?: boolean,
+    customZIndex?: boolean,
+} = {}) => {
    return `
-    bg-[rgba(255,255,255,0.2)]
-    backdrop-blur-[3px]
-    shadow-[rgba(40,40,150,0.35)_8px_8px_32px_0px]
+    ${customBGColor ? "" : "bg-[rgba(255,255,255,0.2)]"}
+    ${customBlurAmt ? "" : "backdrop-blur-[3px]"}
+    ${customShadowColor ? "" : "shadow-[rgba(40,40,150,0.35)]"}
+    ${customShadowSpread ? "" : "shadow-[10px]"}
     bg-gradient-to-br from-[rgba(255,255,255,0.2)] to-[rgba(255,255,255,0)]
     z-[1]
    `.trim()    
